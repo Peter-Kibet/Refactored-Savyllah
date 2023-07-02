6cf64fa13dccfc7666c270c15a99f1500a3ed5ab
Peter-Kibet <peter.kibet@hotmail.com>
2023-07-02 07:43:19 +0300

    Add a functioning login page

---
+++ public/JS/script.js
@@ -0,0 +1,156 @@
+let navbar = document.querySelector(".header .navbar");
+let searchForm = document.querySelector(".header .search-form");
+let loginForm = document.querySelector(".header .login-form");
+let contactInfo = document.querySelector(".contact-info");
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+  searchForm.classList.remove("active");
+  loginForm.classList.remove("active");
+};
+
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.toggle("active");
+  navbar.classList.remove("active");
+  loginForm.classList.remove("active");
+};
+
+document.querySelector("#login-btn").onclick = () => {
+  loginForm.classList.toggle("active");
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+};
+
+document.querySelector("#info-btn").onclick = () => {
+  contactInfo.classList.add("active");
+};
+
+document.querySelector("#close-contact-info").onclick = () => {
+  contactInfo.classList.remove("active");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+  loginForm.classList.remove("active");
+  contactInfo.classList.remove("active");
+};
+
+const homeSlider = new Swiper(".home-slider", {
+  loop: true,
+  grabCursor: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 9000,
+    disableOnInteraction: false,
+  },
+  effect: "cube",
+  cubeEffect: {
+    slidesShadows: true,
+    shadow: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+  },
+  preloadImages: false, // Disable image preloading
+  lazy: true, // Enable lazy loading of images
+  watchSlidesVisibility: true, // Watch slide visibility
+  watchSlidesProgress: true, // Watch slide progress
+});
+
+const reviewsSlider = new Swiper(".reviews-slider", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 20,
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+const blogsSlider = new Swiper(".blogs-slider", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 20,
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+const logoSlider = new Swiper(".logo-slider", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 20,
+  breakpoints: {
+    450: {
+      slidesPerView: 2,
+    },
+    640: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    1000: {
+      slidesPerView: 5,
+    },
+  },
+});
+
+const maxChars = 300; // maximum number of characters to display in the service description
+const ellipsis = "..."; // ellipsis to add at the end of the truncated text
+
+// loop through each service description and truncate the text if necessary
+const serviceDescriptions = document.querySelectorAll(
+  ".blogs .content p, .services .box p"
+);
+serviceDescriptions.forEach((description) => {
+  const text = description.textContent.trim();
+  if (text.length > maxChars) {
+    const truncatedText = text.slice(0, maxChars) + ellipsis;
+    description.textContent = truncatedText;
+  }
+});
+
+const typingText = document.getElementById("typing-text");
+const words = ["Construction", "Architectural"];
+let currentWordIndex = 0;
+
+function typeNextWord() {
+  const currentWord = words[currentWordIndex];
+  const currentWordLength = currentWord.length;
+  let currentLetterIndex = 0;
+
+  function typeNextLetter() {
+    typingText.textContent = currentWord.slice(0, currentLetterIndex + 1);
+    currentLetterIndex++;
+
+    if (currentLetterIndex < currentWordLength) {
+      setTimeout(typeNextLetter, 100);
+    } else {
+      currentWordIndex = (currentWordIndex + 1) % words.length;
+      setTimeout(typeNextWord, 1000);
+    }
+  }
+
+  typeNextLetter();
+}
+
+typeNextWord();
---
+++ server.js
@@ -0,0 +1,115 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const app = express();
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+
+const User = require("./models/user");
+
+const initializePassport = require("./passport-config");
+initializePassport(
+  passport,
+  async (email) => {
+    // Find a user by email in the database
+    return await User.findOne({ email });
+  },
+  async (id) => {
+    // Find a user by ID in the database
+    return await User.findById(id);
+  }
+);
+
+app.use(express.static("public")); // Serve static files from the "public" directory
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "your_secret_here", // Replace 'your_secret_here' with your actual secret value
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method"));
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    process.env.MONGO_URI || "mongodb://localhost:27017/your-database-name",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error(err));
+
+app.set("view engine", "ejs");
+
+app.get("/", checkAuthenticated, (req, res) => {
+  res.render("index.ejs", { name: req.user.name });
+});
+
+app.get("/login", checkNotAuthenticated, (req, res) => {
+  res.render("login.ejs");
+});
+
+app.post(
+  "/login",
+  checkNotAuthenticated,
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
+
+app.get("/register", checkNotAuthenticated, (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", checkNotAuthenticated, async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    await user.save();
+    res.redirect("/login");
+  } catch (error) {
+    console.error(error);
+    res.redirect("/register");
+  }
+});
+
+app.delete("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/login");
+});
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+  next();
+}
+
+app.listen(8000);
